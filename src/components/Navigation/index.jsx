bef08f138668636eb86e77c37f8559c1a8281a15
fevercatdream <kuma18@uw.edu>
2023-06-02 23:29:59 -0700

    added bootstrap navbar to navigation jsx

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,14 +1,42 @@
-function Navigation(){
-    const items = ['About Me', 'Portfolio', 'Contact', 'Resume'];
-    return (
-        <>
-            <ul className="Navigation">
-                {items.map((item) => (
-                    <li key={item}>{item}</li>  
-                ))}
+function Navigation() {
+  const items = ["About Me", "Portfolio", "Contact", "Resume"];
+
+  // event handler
+  const handleClick = (event) => console.log(event);
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Abi Kumagai
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              {items.map((item) => (
+                <li className="nav-item" key={item} onClick={handleClick}>
+                  <a className="nav-link active" aria-current="page" href="#">
+                    {item}
+                  </a>
+                </li>
+              ))}
             </ul>
-        </>
-    );
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export { Navigation };
