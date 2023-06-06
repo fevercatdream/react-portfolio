9eb2091eddf42edae036976fe7c1e81feb73dcbb
fevercatdream <kuma18@uw.edu>
2023-06-05 17:52:58 -0700

    updated footer icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,40 +3,46 @@ import "./style.css";
 
 function Footer() {
   return (
-    <>
+    <footer>
       <a
-        class="icon-link"
+        className="icon-link"
         href="https://github.com/fevercatdream"
         target="_blank"
         rel="noopener"
       >
-        <img class="bi bi-github" aria-hidden="true" src="/images/github.svg" />
+        <img 
+          id="github-id" 
+          className="bi bi-github" 
+          aria-hidden="true" 
+          src="/images/github.svg" />
       </a>
       <a
-        class="icon-link"
+        className="icon-link"
         href="https://www.linkedin.com/in/abi-kumagai-8646b531/"
         target="_blank"
         rel="noopener"
       >
         <img
-          class="bi bi-linkedin"
+          id="linkedin-id"
+          className="bi bi-linkedin"
           aria-hidden="true"
           src="/images/linkedin.svg"
         />
       </a>
       <a
-        class="icon-link"
+        className="icon-link"
         href="https://stackoverflow.com/users/19540062/fevercatdream"
         target="_blank"
         rel="noopener"
       >
         <img
-          class="bi bi-stack-overflow"
+          id="stack-overflow-id"
+          className="bi bi-stack-overflow"
           aria-hidden="true"
           src="/images/stack-overflow.svg"
         />
       </a>
-    </>
+    </footer>
   );
 }
 
