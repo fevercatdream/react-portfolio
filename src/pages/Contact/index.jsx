47e5c11a167834c8533d3a3d6f0a107778f93b6d
fevercatdream <kuma18@uw.edu>
2023-06-07 10:53:07 -0700

    updates

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -12,49 +12,20 @@ function ContactForm() {
 
   return (
     <>
-      <form onSubmit={submitForm}>
+      <form className="needs-validation" novalidate onSubmit={submitForm}>
         <div className="mb-3">
-          <label for="nameFormInput" className="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="nameFormInput"
-            name="name"
-            value={name}
-            placeholder="name"
-          />
+          <label for="nameFormInput" className="form-label">Name</label>
+          <input type="text" className="form-control" id="nameFormInput" name="name" value={name} placeholder="name" />
         </div>
         <div className="mb-3">
-          <label for="emailFormInput" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="emailFormInput"
-            name="email"
-            value={email}
-            placeholder="name@example.com"
-          />
+          <label for="emailFormInput" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="emailFormInput" name="email" value={email} placeholder="name@example.com" />
         </div>
         <div className="mb-3">
-          <label for="messageFormTextarea" className="form-label">
-            Message
-          </label>
-          <textarea
-            className="form-control"
-            id="messageFormTextarea"
-            rows="3"
-            name="message"
-            value={message}
-            placeholder="message"
-          ></textarea>
+          <label for="messageFormTextarea" className="form-label">Message</label>
+          <textarea className="form-control" id="messageFormTextarea" rows="3" name="message" value={message} placeholder="message"></textarea>
         </div>
-        <button className="btn btn-primary" type="submit" value="Submit">
-          Submit
-        </button>
+        <button className="btn btn-primary" type="submit" value="Submit">Submit</button>
       </form>
     </>
   );
